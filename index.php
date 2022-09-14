db2174a69e55ea60bffe2bfb8d23b0806706ff60
kudryanasteysha <kudryastasy@gmail.com>
2022-09-14 20:37:47 +0300

    first commit

--- index.php
+++ index.php
@@ -5,13 +5,24 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Sign Up Form</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css'/>
+        <link rel="stylesheet" href='css/style.css'>
     </head>
     <body>
+        
+    <header>
+	    <div class="overlay">
+            <h1>Simply The Best</h1>
+            <h3>Reasons for Choosing US</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p>
+	        <br>
+            <button>READ MORE</button>
+		</div>
+    </header>
+
         <div class="container mt-4">
 <?php
-    if($_COOKIE['user']==''):
+   if($_COOKIE['user']==''):
 ?>
             <div class="row">
             <div class="col">
@@ -38,11 +49,10 @@
                 </form>
             </div>
         <?php else: ?>
-                <p>Hello <?=$_COOKIE['user']?>. To log out, click on <a href="/exit.php">here </a></p>
+               <p>Hello <?=$_COOKIE['user']?>. To log out, click on <a href="/exit.php">here </a></p>
        <?php endif;?>
 
         </div>
        </div> 
     </body>
 </html>
-
